4ed55380a8224656b7988d11ad4f9be8fe1810b8
Abdourahman Jallow <45719780+KotoJallow@users.noreply.github.com>
2019-01-22 22:23:30 +0300

    Update TestExpressionEvaluator.java

--- ExpressionEvaluator/TestExpressionEvaluator.java
+++ ExpressionEvaluator/TestExpressionEvaluator.java
@@ -1,6 +1,6 @@
 public class TestExpressionEvaluator{
 	public static void main(String[] args){
-		double r =ExpressionEvaluator.evaluate("2+9-8.0/9*5+6-1");
+		double r = ExpressionEvaluator.evaluate("2+9-8.0/9*5+6-1");
 		System.out.println(r);
 	}
 }
